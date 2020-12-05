6f3a89bbe692b75fec4aeada7b20c6a6d3d20d59
Natasha Colusso <natashacolusso@gmail.com>
2020-12-05 00:34:39 +0000

    updating to iclude get balance calculation and isAllowed for transactions specifically withdrawal

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-let balance = 500.00;
+// let balance = 500.00;
 
 class Account {
 
   constructor(username) {
     this.username = username;
-
-    this.balance = 0;
+    this.transactions = [];
+    // this.balance = 0;
   }
+  // **TODOOOO**
+  get balance() {
+    let sum = 0;
+    for (let transaction of this.transactions) {
+      sum += transaction.value;
+    }
+    return sum;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction)
+  }
+
 }
 
 class Transaction {
@@ -15,7 +28,12 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value
+    if (this.isAllowed()) {
+      this.time = new Date()
+      this.account.addTransaction(this);
+    } else {
+      return false;
+    }
   }
 }
 
@@ -24,6 +42,9 @@ class Deposit extends Transaction {
   get value() {
     return this.amount;
   }
+  isAllowed() {
+    return true;
+  }
 }
 
 class Withdrawal extends Transaction {
@@ -31,7 +52,9 @@ class Withdrawal extends Transaction {
   get value() {
     return - this.amount;
   }
-
+  isAllowed() {
+    return this.amount <= this.account.balance;
+  }
 }
 
 
@@ -40,18 +63,44 @@ class Withdrawal extends Transaction {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25, myAccount);
+const myAccount = new Account('billybob');
+
+// console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
+// console.log("TRANSACTIONS: *** ", myAccount.transactions)
+
+const t3 = new Withdrawal(80.00, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
 
-// console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
+
+
+
+
+
+
+
+// const myAccount = new Account("snow-patrol");
+
+// const t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// // console.log('Transaction 1:', t1);
+
+// const t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// // console.log('Transaction 2:', t2);
+
+// const t3 = new Deposit(120.00, myAccount);
+// t3.commit();
+// // console.log('Transaction 3:', t3);
+
+// console.log(myAccount.transactions.transactions)
+
+// // console.log('Balance:', balance);
